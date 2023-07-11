5f882510ddcb4ae30533e01f2513cd2909093070
Fábio Augusto <fabio.augusto1911@gmail.com>
2023-07-11 13:43:02 -0300

    Refactoring fetch data.

--- app/src/main/java/com/ifmg/evolvingpokedex/MainActivity.java
+++ app/src/main/java/com/ifmg/evolvingpokedex/MainActivity.java
@@ -13,32 +13,56 @@ import com.ifmg.evolvingpokedex.classes.getSpecie.PokemonSpecies;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    public void teste(String pokemonName) {
+    public int getPokemon(String pokemonName) throws IOException {
+        int idSpecie = -1;
+
+        PokedexApplication app = (PokedexApplication) getApplication();
+
+        Pokemon pokemon = app.getPokemonRepo().getPokemonByName(pokemonName);
+        String[] aux = pokemon.species.url.split("/");
+
+        if (aux.length >= 1) {
+            idSpecie = Integer.parseInt(aux[aux.length - 1]);
+        }
+
+        return idSpecie;
+    }
+
+    public int getSpecie(int idSpecie) throws IOException {
+        int idEvolutionChain = -1;
+
+        PokedexApplication app = (PokedexApplication) getApplication();
+
+        PokemonSpecies specie = app.getPokemonRepo().getSpecieById(idSpecie);
+        String[] aux = specie.evolutionChain.url.split("/");
+
+        if (aux.length >= 1) {
+            idEvolutionChain = Integer.parseInt(aux[aux.length - 1]);
+        }
+
+        return idEvolutionChain;
+    }
+
+    public void getEvolutions(int idEvolutionChain) throws IOException {
+        PokedexApplication app = (PokedexApplication) getApplication();
+        PokemonEvolution evolution = app.getPokemonRepo().getEvolutionChainById(idEvolutionChain);
+        for (Evolution evo : evolution.chain.evolves_to) {
+            Log.d("POKEDEX-PRINT", evo.species.name);
+        }
+    }
+
+    public void fetchData(String pokemonName) {
         PokedexApplication app = (PokedexApplication) getApplication();
 
         app.executor.execute(() -> {
             try {
-                int idSpecie = -1;
-                int idEvolutionChain = -1;
-                Pokemon pokemon = app.getPokemonRepo().getPokemonByName(pokemonName);
-                String[] aux1 = pokemon.species.url.split("/");
-                if (aux1.length >= 1) {
-                    idSpecie = Integer.parseInt(aux1[aux1.length - 1]);
+                int idSpecie = getPokemon(pokemonName);
 
-                    if (idSpecie != - 1) {
-                        PokemonSpecies specie = app.getPokemonRepo().getSpecieById(idSpecie);
-                        String[] aux2 = specie.evolutionChain.url.split("/");
+                if (idSpecie != -1) {
+                    int idEvolutionChain = getSpecie(idSpecie);
 
-                        if (aux2.length >= 1) {
-                            idEvolutionChain = Integer.parseInt(aux2[aux2.length - 1]);
-
-                            if (idEvolutionChain != - 1) {
-                                PokemonEvolution evolution = app.getPokemonRepo().getEvolutionChainById(idEvolutionChain);
-                                for (Evolution evo : evolution.chain.evolves_to) {
-                                    Log.d("POKEDEX-PRINT", evo.species.name);
-                                }
-                            }
-                        }
+                    if (idEvolutionChain != -1) {
+                        getEvolutions(idEvolutionChain);
                     }
                 }
             } catch (IOException e) {
@@ -47,12 +71,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        teste("eevee");
+        fetchData("mudkip");
     }
 }
